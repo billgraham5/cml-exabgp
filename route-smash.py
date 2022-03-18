fb23fdcec1bdd316655ef0ca7279974a69ba7598
Bill <49030026+w4rfc@users.noreply.github.com>
2022-03-18 08:43:06 -0400

    Additional examples

--- route-smash.py
+++ route-smash.py
@@ -11,7 +11,7 @@ import time
 for first in range(1,223):
 	for second in range(0,255):
 		for third in range(0,255):
-			sys.stdout.write('announce route %d.%d.%d.0/24 next-hop 192.168.100.1\n' % (first, second, third))
+			sys.stdout.write('announce route %d.%d.%d.0/24 next-hop 192.168.99.1\n' % (first, second, third))
 			sys.stdout.flush()
 			## Back off timer if router is too slow:
 			##time.sleep(0.001)
